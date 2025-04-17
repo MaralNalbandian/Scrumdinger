221807739d5ad3306b947ffed6ccde4f2a5e53ff
Maral <37317245+MaralNalbandian@users.noreply.github.com>
2025-04-18 09:03:30 +1000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Maral Nalbandian on 18/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
